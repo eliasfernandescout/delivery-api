4880173858761fb906f3dc435c4ed28092bacc45
elias fernandes <eliasfernandescout@gmail.com>
2021-12-22 16:52:50 -0300

    fix: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response)=>{
-    return console.log('Hello World')
-)}   ;
-
-app.listen(5002, ()=>{
-    console.log('SERVER STARTED ON PORT 5002')
-})
+app.listen(5002, () => {
+    console.log('SERVER STARTED ON PORT 5002');
+});
